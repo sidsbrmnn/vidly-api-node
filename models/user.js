3d4e9583a27158e77d8df1372e44194a2c26eda7
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-04-01 16:00:31 +0530

    Added password hashing using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -5,6 +6,19 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
+});
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+
+    next();
+});
+
+UserSchema.method('checkPassword', async function(password) {
+    const isValidPassword = await bcrypt.compare(password, this.password);
+    return isValidPassword;
 });
 
 const User = mongoose.model('user', UserSchema);
