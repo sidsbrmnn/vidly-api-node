434aae62627f208cce29fec70e292ad07f5d8fdf
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-04-01 15:48:27 +0530

    Created user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
