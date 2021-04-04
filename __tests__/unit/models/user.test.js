ce4f786659745242ae3d8d4d7523c178f8345dfa
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-04-04 13:13:03 +0530

    Update unit test for user.generateAuthToken

--- __tests__/unit/models/user.test.js
+++ __tests__/unit/models/user.test.js
@@ -1,13 +1,12 @@
-const { User } = require('../../../models/user');
-
-const jwt = require('../../../util/jwt');
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/user');
 
 describe('user.generateAuthToken', () => {
-    it('should return a valid JWT', () => {
-        const payload = { isAdmin: true };
-        const user = new User({ ...payload });
-        const token = user.generateAuthToken();
-        const decoded = jwt.verify(token);
-        expect(decoded).toMatchObject(payload);
-    });
+  it('should return a valid JWT', () => {
+    const payload = { name: 'John Doe', role: 'admin' };
+    const user = new User({ ...payload });
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    expect(decoded).toMatchObject(payload);
+  });
 });
