4d411d09e3e3fb8597be768105ea81b6d7d231ef
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-04-27 11:08:24 +0530

    Log all unknown errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -20,6 +20,7 @@ module.exports = function (err, req, res, next) {
   } else if (err instanceof JsonWebTokenError) {
     res.status(401).send({ message: err.message });
   } else {
+    console.log(err);
     res.status(500).send({ message: 'Something went wrong' });
   }
 };
